c8f2e4133ead27ba2a05eec840364c89880d10cf
raihanuldev <rmmahi4770@gmail.com>
2024-02-09 16:00:53 +0600

    Footer added

--- src/Layout/UserLayout.jsx
+++ src/Layout/UserLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nabvar from '../componets/nabavar/Nabvar';
 import { Outlet } from 'react-router-dom';
+import Footer from '../componets/footer/Footer';
 
 const UserLayout = () => {
     return (
         <div className=''>
             <Nabvar/>
             <Outlet/>
+            <Footer/>
         </div>
     );
 };
