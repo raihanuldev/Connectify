5c163dc02350ab7070fd6b3a2478b97586acd181
raihanuldev <rmmahi4770@gmail.com>
2024-02-09 15:58:12 +0600

    Nabvar and Error Page Setup

--- src/Layout/UserLayout.jsx
+++ src/Layout/UserLayout.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Nabvar from '../componets/nabavar/Nabvar';
+import { Outlet } from 'react-router-dom';
 
 const UserLayout = () => {
     return (
-        <div>
-            this is User Layout 
+        <div className=''>
+            <Nabvar/>
+            <Outlet/>
         </div>
     );
 };
