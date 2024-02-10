b6c0e05a35a89db3cae2c559c562c4a8f371fd44
raihanuldev <rmmahi4770@gmail.com>
2024-02-10 16:34:41 +0600

    Top 3 Post

---
+++ src/componets/topPost/TopPost.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import MediaCard from '../mediaCard/MediaCard'
+const TopPost = () => {
+    const [topPost,setTopPost] = useState([])
+
+    useEffect(()=>{
+        fetch('http://localhost:3000/media/top')
+        .then(res=>res.json())
+        .then(data=>setTopPost(data))
+    },[])
+
+    return (
+        <div>
+            <p className='text-center text-3xl font-semibold mt-3'>Todays Trending Post</p>
+            <div className='grid grid-cols-1 md:grid-cols-3 my-4'>
+                {
+                    topPost.map(post=> <MediaCard key={post._id} post={post}/>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default TopPost;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../../componets/banner/Banner';
 import UploadPost from '../../componets/uploadpost/UploadPost';
+import TopPost from '../../componets/topPost/TopPost';
 
 const Home = () => {
     return (
         <div>
             <Banner/>
+            <TopPost/>
         </div>
     );
 };
