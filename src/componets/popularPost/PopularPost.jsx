b450d0d4fbfa6baa21f040d8b68b4370fa0b1486
raihanuldev <rmmahi4770@gmail.com>
2024-02-10 16:40:05 +0600

    popular Post

---
+++ src/componets/popularPost/PopularPost.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import MediaCard from '../mediaCard/MediaCard';
+
+const PopularPost = () => {
+    const [popular, setpopular] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3000/media/popular')
+            .then(res => res.json())
+            .then(data => setpopular(data))
+    }, [])
+
+    return (
+        <div>
+            <p className='text-center text-3xl font-semibold mt-3'>Popular Post</p>
+            <div className='grid grid-cols-1 md:grid-cols-3 my-4'>
+                {
+                    popular.map(post => <MediaCard key={post._id} post={post} />)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default PopularPost;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Banner from '../../componets/banner/Banner';
 import UploadPost from '../../componets/uploadpost/UploadPost';
 import TopPost from '../../componets/topPost/TopPost';
+import PopularPost from '../../componets/popularPost/PopularPost';
 
 const Home = () => {
     return (
         <div>
             <Banner/>
             <TopPost/>
+            <PopularPost/>
         </div>
     );
 };
