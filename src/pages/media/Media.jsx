cd24c6ceca1a6b5371549fd38a8057657ddf4784
raihanuldev <rmmahi4770@gmail.com>
2024-02-10 15:21:40 +0600

    Media page added

--- src/pages/media/Media.jsx
+++ src/pages/media/Media.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import MediaCard from '../../componets/mediaCard/MediaCard';
 
 const Media = () => {
+    const [media, setMedia] = useState([]);
+    console.log(media)
+    useEffect(() => {
+        fetch('http://localhost:3000/media')
+            .then(res => res.json())
+            .then(data => setMedia(data))
+    }, [])
     return (
         <div>
-            this is media page
+            <h2 className='text-center text-2xl font-sans font-semibold my-3'>Explore All Media</h2>
+            <div className='grid grid-cols-3'>
+                {
+                    media.map(post => <MediaCard key={post.id} post={post}/>)
+                }
+            </div>
         </div>
     );
 };
